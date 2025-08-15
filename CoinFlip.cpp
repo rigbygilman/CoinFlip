edf1b93e1d6b1e8a5a87b4295d865d2d689c2ff9
Rigby <rigby.2593046@nv.ccsd.net>
2025-08-15 12:54:39 -0700

    optimise

--- CoinFlip.cpp
+++ CoinFlip.cpp
@@ -4,14 +4,9 @@ using namespace std;
 int main() {
     srand(time(0)); // initializing the random number generator
     
-    // init vars
-    int num = (rand() % 2); // 1 is heads, 0 is tails
-    bool heads; 
-    
-    // cheking if its heads or not
-    if (num == 1) heads = true;
-    else heads = false;
-    
+    // init var
+    bool heads = ((rand() % 2) == 1 ? true : false);
     cout << (heads ? "heads!" : "tails!"); // printing answer
+    
     return 0;
 }
